8ad4e3a8a14334e305182a69c3c34c9419fc8d09
Clay Sweetser <clay.sweetser@gmail.com>
2012-02-01 17:12:27 -0500

    Started on SmartDashboard additions to subsystems. LeverSystem now sends Jaguar motors PWM to SmartDashBoard, and GyroSystem now sends Gyro data. NOTE - UNTESTED CODE.

--- LokiProgram/src/edu/wpi/first/wpilibj/templates/subsystems/GyroSystem.java
+++ LokiProgram/src/edu/wpi/first/wpilibj/templates/subsystems/GyroSystem.java
@@ -1,6 +1,7 @@
 package edu.wpi.first.wpilibj.templates.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
 import edu.wpi.first.wpilibj.smartdashboard.SendableGyro;
 
 /**
@@ -15,7 +16,19 @@ public class GyroSystem extends Subsystem {
         xSendableGyro = new SendableGyro(1);
         ySendableGyro = new SendableGyro(1);
     }
+    private NetworkTable table;
 
+    public NetworkTable getTable() {
+        if (table == null) {
+            table = super.getTable();
+            table.putSubTable("xGyro", xSendableGyro.getTable());
+            table.putSubTable("yGyro", ySendableGyro.getTable());
+        }
+        return table;
+    }
+
+    ;
+    
     public void initDefaultCommand() {
         // Set the default command for a subsystem here. This should probably be something that sends the gyro values to the dashboard
         //setDefaultCommand(new MySpecialCommand());
