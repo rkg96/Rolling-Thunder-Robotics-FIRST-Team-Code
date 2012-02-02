40de36d67b093850009697ced0b4c1340b2c8e8a
Giang <Giang@192.168.27.108>
2012-02-01 20:46:25 -0500

    initial baseline

--- LokiProgram/src/edu/wpi/first/wpilibj/templates/subsystems/GyroSystem.java
+++ LokiProgram/src/edu/wpi/first/wpilibj/templates/subsystems/GyroSystem.java
@@ -7,7 +7,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableGyro;
  *
  */
 public class GyroSystem extends Subsystem {
-
+double gyroAngle=0.0;
     public SendableGyro SendableGyro;
     //public SendableGyro ySendableGyro;
 
@@ -15,7 +15,12 @@ public class GyroSystem extends Subsystem {
         SendableGyro = new SendableGyro(1);
        // ySendableGyro = new SendableGyro(1);
     }
-
+public double getAngle(){
+    return gyroAngle;
+}
+public void updateAngle(double angle){
+    gyroAngle+=angle;
+}
     public void initDefaultCommand() {
         // Set the default command for a subsystem here. This should probably be something that sends the gyro values to the dashboard
         //setDefaultCommand(new MySpecialCommand());
